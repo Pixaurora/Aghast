648e48ec5d372ce2440b7b7eec454559ad3fc75c
Pixirora <rina@pixirora.dev>
2024-02-28 18:12:15 -0500

    personalize project

--- client/src/main/java/net/pixaurora/aghast/mixin/MinecraftMixin.java
+++ client/src/main/java/net/pixaurora/aghast/mixin/MinecraftMixin.java
@@ -1,19 +1,17 @@
-package com.example.example_mod.mixin;
+package net.pixaurora.aghast.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.example.example_mod.ExampleMod;
-
 import net.minecraft.client.Minecraft;
+import net.pixaurora.aghast.AghastMod;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-
 	@Inject(method = "main", remap = false, at = @At("HEAD"))
 	public static void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		AghastMod.LOGGER.info("This line is printed by a ghast mod mixin!");
 	}
 }
